9c6f30ee9617afb6d2c1aaab78539b4a9f0a6c0f
Abhinav Jauhari <abhinav.jauhari@gmail.com>
2023-06-04 20:11:56 +0530

    Modified

--- Node.js/myNode.js
+++ Node.js/myNode.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var myMod = require('./MyModule.js');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.write("The date and time are currently: " + myMod.myDateTime());
+  res.end();
 }).listen(8080);
